e28966667e3b2f9406a356908cbc89791a1d9a80
Bhoovi Verma <bhooviverma02@gmail.com>
2026-06-02 13:43:00 +0530

    Add files via upload

---
+++ Q_338.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* countBits(int n, int* returnSize) {
+    *returnSize = n + 1;
+
+    int* ans = (int*)malloc((n + 1) * sizeof(int));
+
+    for (int i = 0; i <= n; i++) {
+        int num = i;
+        int count = 0;
+
+        while (num > 0) {
+            count += num % 2;
+            num /= 2;
+        }
+
+        ans[i] = count;
+    }
+
+    return ans;
+}
